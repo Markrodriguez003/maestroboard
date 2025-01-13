04ea22e3e58c605df324b06960abe7751731b8d0
Mark Rodriguez <Markrodriguez003@gmail.com>
2025-01-13 14:17:53 -0500

    Added page-not-found page route

--- src/App.jsx
+++ src/App.jsx
@@ -9,6 +9,7 @@ import Login from "./components/pages/Login";
 import News from "./components/pages/News";
 import CommunityBoard from "./components/pages/CommunityBoard";
 import Forum from "./components/Forum";
+import PageNotFound from "./components/pages/PageNotFound";
 // ! change to home
 import IntroSplashPage from "./components/IntroSplashPage";
 import Dashboard from "./components/pages/Dashboard";
@@ -34,6 +35,12 @@ function App() {
         <Route path="/login" element={<Login />}></Route>
         <Route path="/signup" element={<SignUpForm />}></Route>
         <Route path="/dashboard" element={<Dashboard />}></Route>
+        <Route
+          path="*"
+          element={<PageNotFound />}
+        />
+
+
       </Routes>
 
       <Footer />
---
+++ src/components/pages/PageNotFound.jsx
@@ -0,0 +1,52 @@
+
+
+// ASSETS
+import { FileEarmarkExcelFill } from "react-bootstrap-icons"
+
+
+
+
+// COMPONENTS
+import { useState } from "react";
+import { Row, Col, Form, Button, Container, Spinner, Toast } from "react-bootstrap";
+
+// ASSETS
+import { PersonFillLock } from "react-bootstrap-icons";
+
+// CSS
+import { SITE_COLORS } from "../css/site";
+// import "../css/Login.css";
+
+function PageNotFound(props) {
+
+
+
+    return (
+
+        <>
+            <Container className="col-11 col-lg-5 col-md-7 p-5 mt-4 pb-5 rounded-4 shadow-lg" style={{ backgroundColor: SITE_COLORS.main }}>
+
+                <Row className="p-0 m-0 text-center gap-0" style={{ color: "white" }}>
+                    <FileEarmarkExcelFill style={{ fontSize: "150px" }} className="mb-4" />
+                    <hr />
+                    <h1>404: PAGE NOT FOUND</h1>
+                    <p>Hmm..</p>
+                    <p>Sorry, that page cannot be found. </p>
+                    <p>Please check the URL as the page may have moved </p>
+                    {/* <br /> */}
+                    <Button className="mt-3" href="/home" >Go Back Home</Button>
+                </Row>
+            </Container >
+            <br />
+            <br />
+            <br />
+
+        </>
+
+    );
+}
+
+export default PageNotFound;
+
+
+
