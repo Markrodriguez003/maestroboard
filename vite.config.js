0f9a326fa1a7d1d08a6d77310d66658bc3e00d3b
Mark Rodriguez <Markrodriguez003@gmail.com>
2025-02-26 16:52:54 -0500

    Adjusted vite config file

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,14 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
+// export default defineConfig({
+//   plugins: [react()],
+// })
 export default defineConfig({
   plugins: [react()],
+  server: {
+  host: true,
+  strictPort: true,
+  port: 3005,
+  },
 })
